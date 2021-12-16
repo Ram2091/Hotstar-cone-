7bdc1707164e60d8fedd5ff3412cd8339df8347d
Ramkishore  T <kishorethangavel55@gmail.com>
2021-12-16 19:13:38 +0530

    Hotstar

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let sliders = [];
 let slideIndex = 0; // to track current slide index.
 
 const createSlide = () => {
-    if(slideIndex >= movies.length){
+    if (slideIndex >= movies.length) {
         slideIndex = 0;
     }
 
@@ -15,17 +15,18 @@ const createSlide = () => {
     let h1 = document.createElement('h1');
     let p = document.createElement('p');
 
-     // attaching all elements
-     imgElement.appendChild(document.createTextNode(''));
-     h1.appendChild(document.createTextNode(movies[slideIndex].name));
-     p.appendChild(document.createTextNode(movies[slideIndex].des));
-     content.appendChild(h1);
-     content.appendChild(p);
-     slide.appendChild(content);
-     slide.appendChild(imgElement);
-     carousel.appendChild(slide);
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
 
- // setting up image
+    // setting up image
     imgElement.src = movies[slideIndex].image;
     slideIndex++;
 
@@ -36,17 +37,52 @@ const createSlide = () => {
     p.className = 'movie-des';
 
     sliders.push(slide);
-//adding sliding effect
-    if(sliders.length){
+    //adding sliding effect
+    if (sliders.length) {
         sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
     }
 
+}
+for (let i = 0; i < 3; i++) {
+    createSlide();
+}
 
-    }
-    for(let i = 0; i < 3; i++){
-        createSlide();
-    }
-    
-    setInterval(() => {
-        createSlide();
-    }, 3000);
+setInterval(() => {
+    createSlide();
+}, 
+2000);
+
+
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
+
+
+
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
